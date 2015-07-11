6462c905f510b38ff37fa6369571799616facc1e
Jason El-Massih <jel-massih@hotmail.com>
2015-07-10 21:37:49 -0400

    Add Core allocator and types

--- src/core/Allocator.h
+++ src/core/Allocator.h
@@ -15,6 +15,15 @@ namespace core
 		virtual ~Allocator() {}
 
 		virtual void* Allocate(uint32_t size, uint32_t align = DEFAULT_ALIGN) = 0;
+		
+		virtual void Free(void* p) = 0;
+
+		virtual uint32_t GetAllocatedSize(void* p) = 0;
+		virtual uint32_t GetTotalAllocated() = 0;
+
+	private:
+		Allocator(const Allocator& other);
+		Allocator& operator*(const Allocator& other);
 	};
 }
 
