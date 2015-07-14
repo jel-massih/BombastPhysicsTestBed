99967c895afbe87a855b5d542bac4459e2a5b8b9
Jason El-Massih <jel-massih@hotmail.com>
2015-07-14 03:00:38 -0400

    WIP Thinghy

--- src/graphics/ShaderManager.cpp
+++ src/graphics/ShaderManager.cpp
@@ -1,6 +1,8 @@
 #include "ShaderManager.h"
 #include "Core.h"
 
+#include <Windows.h>
+
 ShaderManager::ShaderManager()
 {
 	Load();
@@ -21,15 +23,27 @@ void ShaderManager::Reload()
 uint32_t ShaderManager::GetShader(const char* name)
 {
 	const uint32_t key = hashString(name);
-	return key;
+	uint32_t shader = m_shaders.at(key);
+
+	if (shader)
+	{
+		return shader;
+	}
+
+	const uint32_t defaultKey = hashString("default");
+	return m_shaders.at(defaultKey);
 }
 
 void ShaderManager::Load()
 {
+	const char* shaderDirectory = "data/shaders";
 
+	WIN32_FIND_DATA ffd;
+	HANDLE hFind = INVALID_HANDLE_VALUE;
+
+	hFind = FindFirstFile(shaderDirectory, &ffd);
 }
 
 void ShaderManager::Unload()
 {
-
 }
